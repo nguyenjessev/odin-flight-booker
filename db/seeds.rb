a370a73e27ca77d1d6f418d23484c9981a685ca0
Jesse <nguyenjessev@gmail.com>
2021-06-18 18:31:32 -0400

    Seed with Flights

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Flight.destroy_all
 Airport.destroy_all
+
 Airport.create(code: 'ATL')
 Airport.create(code: 'DFW')
 Airport.create(code: 'DEN')
 Airport.create(code: 'ORD')
 Airport.create(code: 'LAX')
+
+Flight.create(from_airport_id: Airport.first.id, to_airport_id: Airport.second.id, start: DateTime.new(2021, 12, 12, 8, 30), duration_in_mins: 150)
+Flight.create(from_airport_id: Airport.first.id, to_airport_id: Airport.third.id, start: '2021-12-10 07:30:00.000', duration_in_mins: 180)
+Flight.create(from_airport_id: Airport.second.id, to_airport_id: Airport.fourth.id, start: '2021-12-10 07:30:00.000', duration_in_mins: 140)
+Flight.create(from_airport_id: Airport.fifth.id, to_airport_id: Airport.first.id, start: '2021-10-10 15:30:00.000', duration_in_mins: 250)
+Flight.create(from_airport_id: Airport.fifth.id, to_airport_id: Airport.fourth.id, start: '2021-08-10 19:30:00.000', duration_in_mins: 235)
