74cfd0ee00bf77cd721081988a7ec81628228d39
Jesse <nguyenjessev@gmail.com>
2021-06-18 23:52:18 -0400

    Return search results

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,16 @@
 class FlightsController < ApplicationController
   def index
-    @date_options = Flight.select(:start).distinct
+    return unless %i[origin_id destination_id passengers departing].all? { |k| params.key?(k) }
+
+    @flights = Flight.where('from_airport_id = :origin_id
+                            AND to_airport_id = :destination_id
+                            AND start_date = :departing',
+                            search_params.to_h)
+  end
+
+  private
+
+  def search_params
+    params.permit(:origin_id, :destination_id, :passengers, :departing)
   end
 end
