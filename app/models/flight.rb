fcded597de0d9429b74610e8b1d2cbafe350b39c
Jesse <nguyenjessev@gmail.com>
2021-06-20 13:49:13 -0400

    Show flight data in search results

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,11 @@
 class Flight < ApplicationRecord
   belongs_to :from_airport, class_name: 'Airport'
   belongs_to :to_airport, class_name: 'Airport'
+
+  def readable_duration
+    hours = duration_in_mins / 60
+    minutes = duration_in_mins % 60
+
+    "#{hours}H #{minutes}M"
+  end
 end
